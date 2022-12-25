af2d9a75b964f64d11482b343b4f0739ac87c3a1
정형진 <88423849+KeMezz@users.noreply.github.com>
2022-12-25 13:49:48 +0900

    implements logout

--- screens/My.tsx
+++ screens/My.tsx
@@ -1,10 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import React from "react";
+import { useSetRecoilState } from "recoil";
 import styled from "styled-components/native";
+import { loginAtom } from "../utils/recoil";
 
 const My: React.FC = () => {
+  const setIsLoggedIn = useSetRecoilState(loginAtom);
+  const onLogoutPress = () => {
+    AsyncStorage.clear();
+    setIsLoggedIn(false);
+  };
   return (
     <Container>
-      <Text>My</Text>
+      <Logout onPress={onLogoutPress}>
+        <Text>로그아웃</Text>
+      </Logout>
     </Container>
   );
 };
@@ -13,6 +23,9 @@ const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+`;
+const Logout = styled.TouchableOpacity`
+  padding: 20px;
 `;
 const Text = styled.Text`
   color: ${({ theme }) => theme.textColor};
