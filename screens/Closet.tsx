a41d93d40f747175e64d0077704ba5bb855d0b45
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-10 18:09:01 +0900

    옷장 상단부 개발

--- screens/Closet.tsx
+++ screens/Closet.tsx
@@ -1,30 +1,93 @@
-import { useNavigation } from "@react-navigation/native";
-import React from "react";
+import React, { useState } from "react";
+import { Modal } from "react-native";
 import styled from "styled-components/native";
+import ClosetTitle from "../components/ClosetTitle";
+import { Ionicons } from "@expo/vector-icons";
+
+type optionType = "카테고리별" | "이러쿵저러쿵별" | "어쩔티비별";
+const options: optionType[] = ["카테고리별", "이러쿵저러쿵별", "어쩔티비별"];
 
 const Closet: React.FC = () => {
-  const { navigate } = useNavigation();
-  const pickNextDress = () => {
-    // @ts-ignore
-    navigate("Stack", { screen: "PickNextDress" });
+  const [selectedOption, setSelectedOption] =
+    useState<optionType>("카테고리별");
+  const [modalVisible, setModalVisible] = useState(false);
+  const showModal = () => setModalVisible(true);
+  const closeModal = () => setModalVisible(false);
+  const onOptionPressed = (option: optionType) => {
+    console.log(option);
+    // setSelectedOption(option);
+    // closeModal();
   };
   return (
     <Container>
-      <Button onPress={pickNextDress}>
-        <Text>내일 입을 옷 추가하기</Text>
-      </Button>
+      <Modal animationType="fade" transparent visible={modalVisible}>
+        <ModalContainer>
+          <ModalBox>
+            {options.map((option, i) => (
+              <ModalOption
+                key={i}
+                onPress={(option) => onOptionPressed(option)}
+              >
+                <Text>{option}</Text>
+              </ModalOption>
+            ))}
+          </ModalBox>
+          <Overlay onPress={closeModal} />
+        </ModalContainer>
+      </Modal>
+      <ClosetTitle />
+      <Sorter>
+        <Text>어떻게 보여드릴까요?</Text>
+        <Select onPress={showModal}>
+          <SelectText>{selectedOption}</SelectText>
+          <Ionicons name="chevron-down" size={18} color="black" />
+        </Select>
+      </Sorter>
     </Container>
   );
 };
 
 const Container = styled.View`
+  padding: 22px;
+`;
+const ModalContainer = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
 `;
-const Button = styled.TouchableOpacity``;
-const Text = styled.Text`
-  color: ${({ theme }) => theme.textColor};
+const Overlay = styled.Pressable`
+  position: absolute;
+  flex: 1;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+`;
+const ModalBox = styled.View`
+  z-index: 1;
+  width: 300px;
+  background-color: #fff;
+  border-radius: 8px;
+`;
+const ModalOption = styled.TouchableOpacity`
+  width: 100%;
+  padding: 16px;
+`;
+const Sorter = styled.View`
+  margin-top: 18px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+const Text = styled.Text``;
+const Select = styled.TouchableOpacity`
+  align-items: center;
+  flex-direction: row;
+  border: solid 1px ${({ theme }) => theme.textColor};
+  padding: 6px 12px;
+  border-radius: 8px;
+`;
+const SelectText = styled.Text`
+  margin-right: 8px;
 `;
 
 export default Closet;
