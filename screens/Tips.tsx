0883ebee016b12addb1f1db14725f59b50453710
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-01-19 23:47:09 +0900

    tips 대략 외관 작성

--- screens/Tips.tsx
+++ screens/Tips.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Appbar, Text } from "react-native-paper";
+import { FlatList, ScrollView, StyleSheet, View } from "react-native";
+import { Appbar, Card, Text } from "react-native-paper";
 
 const Tips: React.FC = () => {
   return (
@@ -8,18 +8,49 @@ const Tips: React.FC = () => {
       <Appbar.Header>
         <Appbar.Content title="Tips" />
       </Appbar.Header>
-      <View style={styles.container}>
-        <Text>Tips</Text>
-      </View>
+      <ScrollView style={styles.scrollView}>
+        <Card onPress={() => {}} style={styles.card}>
+          <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+          <Card.Title
+            title="더 맛깔나는 목도리 스타일링"
+            titleStyle={{ fontWeight: "600", marginBottom: 3 }}
+            subtitle="한 겨울에 깔끔하게! 목도리 매는 법 11가지"
+            titleVariant="titleLarge"
+            style={{ marginVertical: 8 }}
+          />
+        </Card>
+        <Card onPress={() => {}} style={styles.card}>
+          <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+          <Card.Title
+            title="항상 어려웠던 장례식 복장 예절"
+            titleStyle={{ fontWeight: "600", marginBottom: 3 }}
+            subtitle="조문객의 옷차림을 알려드릴게요. 갑작스러운 문상에도 당황하지 마세요."
+            titleVariant="titleLarge"
+            style={{ marginVertical: 8 }}
+          />
+        </Card>
+        <Card onPress={() => {}} style={styles.card}>
+          <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+          <Card.Title
+            title="더 맛깔나는 목도리 스타일링"
+            titleStyle={{ fontWeight: "600", marginBottom: 3 }}
+            subtitle="한 겨울에 깔끔하게! 목도리 매는 법 11가지"
+            titleVariant="titleLarge"
+            style={{ marginVertical: 8 }}
+          />
+        </Card>
+      </ScrollView>
     </>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+  scrollView: {
+    padding: 16,
+    paddingTop: 0,
+  },
+  card: {
+    marginBottom: 16,
   },
 });
 
