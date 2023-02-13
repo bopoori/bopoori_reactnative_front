8f650f964a801fc75798d01299867ae74abbad35
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-13 16:10:39 +0900

    내 정보 외관 작성

--- screens/settings/MyInformation.tsx
+++ screens/settings/MyInformation.tsx
@@ -1,16 +1,80 @@
 import { useNavigation } from "@react-navigation/native";
-import { Appbar } from "react-native-paper";
+import { useState } from "react";
+import {
+  Appbar,
+  Button,
+  Dialog,
+  List,
+  Portal,
+  TextInput as TextInputPaper,
+} from "react-native-paper";
+import styled from "styled-components/native";
+import { TextInput } from "react-native";
 
 const MyInformation = () => {
   const { goBack } = useNavigation();
+  const [showDialog, setShowDialog] = useState(false);
+  const [nickname, setNickname] = useState("멋쟁이");
+  const onChangeNickname = (text: string) => setNickname(text);
+  const openDialog = () => setShowDialog(true);
+  const closeDialog = () => setShowDialog(false);
   return (
     <>
+      <Portal>
+        <Dialog visible={showDialog} onDismiss={closeDialog}>
+          <Dialog.Title>닉네임 변경</Dialog.Title>
+          <Dialog.Content>
+            {/* <TextInput
+              defaultValue={nickname}
+              style={{ backgroundColor: "white", padding: 8, borderRadius: 12 }}
+            /> */}
+            <TextInputPaper
+              label="새로운 닉네임 입력"
+              value={nickname}
+              onChangeText={(text: string) => onChangeNickname(text)}
+            />
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={closeDialog}>닫기</Button>
+            <Button>변경하기</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
       <Appbar.Header>
         <Appbar.BackAction onPress={goBack} />
         <Appbar.Content title="내 정보" />
       </Appbar.Header>
+      <Container>
+        <ListSection>
+          <ListItem
+            descriptionStyle={{ paddingTop: 8 }}
+            title="닉네임"
+            description={nickname}
+            onPress={openDialog}
+          />
+        </ListSection>
+        <Wrapper>
+          <Button onPress={() => {}}>비밀번호 변경</Button>
+          <Button labelStyle={{ color: "crimson" }} onPress={() => {}}>
+            회원 탈퇴
+          </Button>
+        </Wrapper>
+      </Container>
     </>
   );
 };
+
+const Container = styled.View``;
+const ListSection = styled(List.Section)`
+  padding-top: 0;
+`;
+const ListItem = styled(List.Item)`
+  padding-left: 8px;
+`;
+const Wrapper = styled.View`
+  justify-content: center;
+  align-items: center;
+  margin-top: 24px;
+`;
 
 export default MyInformation;
