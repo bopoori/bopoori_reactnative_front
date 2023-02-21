f8a34292ec5a9b975ae6d2e12704a319397787fe
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-21 22:11:13 +0900

    useMutation 통해 메일 발송 API 통신

--- screens/stacks/SignUp.tsx
+++ screens/stacks/SignUp.tsx
@@ -1,23 +1,43 @@
+import React from "react";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { useState } from "react";
 import { Appbar, Button, Divider, TextInput } from "react-native-paper";
 import styled from "styled-components/native";
 import { StackParamList } from "../../navigation/Stack";
 import { Alert, KeyboardAvoidingView, Platform } from "react-native";
 import { useForm } from "react-hook-form";
 import ControlledInput from "../../components/ControlledInput";
+import { useMutation } from "@tanstack/react-query";
+import { getOtp } from "../../utils/api";
 
 type Props = NativeStackScreenProps<StackParamList, "SignUp">;
 
-const SignUp: React.FC<Props> = ({ navigation: { goBack, navigate } }) => {
-  const { control, handleSubmit } = useForm();
-  const [otpSented, setOtpSented] = useState(false);
-  const [otp, setOtp] = useState("");
-  const onChangeOtp = (text: string) => setOtp(text);
+interface SignUpForm {
+  user_nickname: string;
+  user_email: string;
+}
 
-  const sendEmail = () => {
-    Alert.alert("메일을 발송했습니다.");
-    setOtpSented(true);
+const SignUp: React.FC<Props> = ({ navigation: { goBack, navigate } }) => {
+  const { control, handleSubmit } = useForm<SignUpForm>();
+  const [otp, setOtp] = React.useState("");
+  const [otpSented, setOtpSented] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const { mutateAsync } = useMutation((user_email: string) =>
+    getOtp(user_email)
+  );
+
+  const onChangeOtp = (text: string) => setOtp(text);
+  const sendEmail = (formData: SignUpForm) => {
+    setLoading(true);
+    mutateAsync(formData.user_email).then(({ success, message }) => {
+      setLoading(false);
+      if (success) {
+        setOtpSented(true);
+        Alert.alert("메일을 발송했습니다.");
+      } else {
+        Alert.alert(message);
+      }
+    });
   };
 
   const certEmail = () => {
@@ -38,6 +58,7 @@ const SignUp: React.FC<Props> = ({ navigation: { goBack, navigate } }) => {
         <Container>
           <FormBox>
             <ControlledInput
+              disabled={otpSented}
               label="닉네임"
               name="user_nickname"
               autoComplete="name"
@@ -46,15 +67,21 @@ const SignUp: React.FC<Props> = ({ navigation: { goBack, navigate } }) => {
           </FormBox>
           <FormBox>
             <ControlledInput
+              disabled={otpSented}
               label="이메일"
               name="user_email"
               autoComplete="email"
               control={control}
+              pattern={{
+                value: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                message: "이메일 형식이 올바르지 않습니다.",
+              }}
             />
           </FormBox>
           <Button
             disabled={otpSented}
             onPress={handleSubmit(sendEmail)}
+            loading={loading}
             style={{ alignSelf: "center" }}
           >
             인증 번호 보내기
