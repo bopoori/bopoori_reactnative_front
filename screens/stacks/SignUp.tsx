02450c85cc9e5264d4363022c7be96bf099ed756
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-21 13:57:54 +0900

    디자인 다듬기
    - keyboard avoiding view 적용
    - 버튼에 아이콘 추가
    - 엘리먼트 간 간격 조절

--- screens/stacks/SignUp.tsx
+++ screens/stacks/SignUp.tsx
@@ -1,16 +1,10 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useState } from "react";
-import {
-  Appbar,
-  Button,
-  Menu,
-  SegmentedButtons,
-  Text,
-  TextInput,
-} from "react-native-paper";
+import { Appbar, Button, Divider, Text, TextInput } from "react-native-paper";
 import styled from "styled-components/native";
 import { StackParamList } from "../../navigation/Stack";
 import { Ionicons } from "@expo/vector-icons";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 type Props = NativeStackScreenProps<StackParamList, "SignUp">;
 
@@ -22,72 +16,73 @@ const SignUp: React.FC<Props> = ({ navigation: { goBack } }) => {
         <Appbar.BackAction onPress={goBack} />
         <Appbar.Content title="회원가입" />
       </Appbar.Header>
-      <Form>
-        <FormBox>
-          <Input label="닉네임" mode="outlined" />
-        </FormBox>
-        <FormBox>
-          <Input label="이메일" mode="outlined" />
-        </FormBox>
-        <FormBox>
-          <Input
-            label="키"
-            mode="outlined"
-            keyboardType="numeric"
-            right={<TextInput.Affix text="cm" />}
-          />
-        </FormBox>
-        <FormBox>
-          <Input
-            label="몸무게"
-            mode="outlined"
-            keyboardType="numeric"
-            right={<TextInput.Affix text="kg" />}
-          />
-        </FormBox>
-        <FormBox>
-          {/* <Input label="성별" mode="outlined" keyboardType="numeric" /> */}
-          {/* <SegmentedButtons
-            value={gender}
-            onValueChange={setGender}
-            buttons={[
-              {
-                value: "female",
-                label: "여성",
-              },
-              {
-                value: "male",
-                label: "남성",
-              },
-            ]}
-          /> */}
-          <ButtonWrapper>
-            <Button
-              style={{ width: "100%" }}
-              mode={gender === "female" ? "contained-tonal" : "outlined"}
-              onPress={() => setGender("female")}
-            >
-              여성
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <Form>
+          <FormBox>
+            <Input label="닉네임" mode="outlined" />
+          </FormBox>
+          <FormBox>
+            <Input label="이메일" mode="outlined" />
+          </FormBox>
+          <Divider style={{ marginTop: 20, marginBottom: 10 }} />
+          <FormBox>
+            <ButtonWrapper>
+              <Button
+                style={{ width: "100%" }}
+                mode={gender === "female" ? "contained-tonal" : "outlined"}
+                onPress={() => setGender("female")}
+                icon="face-woman"
+              >
+                여성
+              </Button>
+            </ButtonWrapper>
+            <ButtonWrapper>
+              <Button
+                style={{ width: "100%" }}
+                mode={gender === "male" ? "contained-tonal" : "outlined"}
+                onPress={() => setGender("male")}
+                icon="face-man"
+              >
+                남성
+              </Button>
+            </ButtonWrapper>
+          </FormBox>
+          <FormBox>
+            <Input
+              label="키"
+              mode="outlined"
+              keyboardType="number-pad"
+              returnKeyType="done"
+              right={<TextInput.Affix text="cm" />}
+            />
+          </FormBox>
+          <FormBox>
+            <Input
+              label="몸무게"
+              mode="outlined"
+              keyboardType="number-pad"
+              returnKeyType="done"
+              right={<TextInput.Affix text="kg" />}
+            />
+          </FormBox>
+          <AlertText>
+            <Ionicons
+              name="alert-circle"
+              size={22}
+              style={{ marginRight: 8 }}
+            />
+            <Text>이후 스타일링 추천을 위해 키와 몸무게를 알려주세요</Text>
+          </AlertText>
+          <ConfirmWrapper>
+            <Button mode="contained" onPress={() => {}}>
+              가입하기
             </Button>
-          </ButtonWrapper>
-          <ButtonWrapper>
-            <Button
-              style={{ width: "100%" }}
-              mode={gender === "male" ? "contained-tonal" : "outlined"}
-              onPress={() => setGender("male")}
-            >
-              남성
-            </Button>
-          </ButtonWrapper>
-        </FormBox>
-        <AlertText>
-          <Ionicons name="alert-circle" size={22} style={{ marginRight: 8 }} />
-          <Text>이후 스타일링 추천을 위해 키와 몸무게를 알려주세요</Text>
-        </AlertText>
-        <ConfirmWrapper>
-          <Button mode="contained">가입하기</Button>
-        </ConfirmWrapper>
-      </Form>
+          </ConfirmWrapper>
+        </Form>
+      </KeyboardAvoidingView>
     </>
   );
 };
