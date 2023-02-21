97aaa9d31c533b86025bbf06bdded397de50292e
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-02-21 19:42:31 +0900

    주석 및 안쓰는 변수 삭제

--- screens/stacks/SignUp.tsx
+++ screens/stacks/SignUp.tsx
@@ -77,63 +77,6 @@ const SignUp: React.FC<Props> = ({ navigation: { goBack, navigate } }) => {
               인증하기
             </Button>
           </ConfirmWrapper>
-          {/* <Divider style={{ marginTop: 20, marginBottom: 10 }} />
-          <GenderWrapper>
-            <GenderBox>
-              <ButtonWrapper>
-                <Button
-                  style={{ width: "100%" }}
-                  mode={gender === "female" ? "contained-tonal" : "outlined"}
-                  onPress={() => setGender("female")}
-                  icon="face-woman"
-                >
-                  여성
-                </Button>
-              </ButtonWrapper>
-              <ButtonWrapper>
-                <Button
-                  style={{ width: "100%" }}
-                  mode={gender === "male" ? "contained-tonal" : "outlined"}
-                  onPress={() => setGender("male")}
-                  icon="face-man"
-                >
-                  남성
-                </Button>
-              </ButtonWrapper>
-            </GenderBox>
-          </GenderWrapper>
-          <FormBox>
-            <ControlledInput
-              label="키"
-              name="user_height"
-              control={control}
-              numberPad
-              rightText="cm"
-            />
-          </FormBox>
-          <FormBox>
-            <ControlledInput
-              label="몸무게"
-              name="user_weight"
-              control={control}
-              numberPad
-              rightText="kg"
-            />
-          </FormBox>
-          <AlertText>
-            <Ionicons
-              name="alert-circle"
-              color={isDark ? "#fff" : "#000"}
-              size={22}
-              style={{ marginRight: 8 }}
-            />
-            <Text>이후 스타일링 추천을 위해 키와 몸무게를 알려주세요</Text>
-          </AlertText>
-          <ConfirmWrapper>
-            <Button mode="contained" onPress={handleSubmit(onSubmit)}>
-              가입하기
-            </Button>
-          </ConfirmWrapper> */}
         </Container>
       </KeyboardAvoidingView>
     </>
@@ -148,22 +91,6 @@ const DividerWrapper = styled.View`
 `;
 const FormBox = styled.View`
   min-height: 70px;
-`;
-const ButtonWrapper = styled.View`
-  width: 48%;
-`;
-const GenderBox = styled.View`
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  height: 70px;
-`;
-const GenderWrapper = styled.View``;
-const AlertText = styled.View`
-  margin: 0 auto;
-  margin-top: 20px;
-  flex-direction: row;
-  align-items: center;
 `;
 const ConfirmWrapper = styled.View`
   margin-top: 10px;
