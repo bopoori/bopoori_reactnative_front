2b4019640a4aabb1e48f765dc3fc9ebda06e897f
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-02 21:44:05 +0900

    fetch 대신 axios로 변경

--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,11 @@
+import axios from "axios";
+
 const BASE_URL = "http://3.39.118.55:12023";
 
+axios.defaults.headers.common["Content-Type"] =
+  "application/json; charset=utf-8";
+
 export const getOtp = (user_email: string) =>
-  fetch(`${BASE_URL}/bopool/auth/registration/mail`, {
-    headers: { "Content-Type": "application/json; charset=utf-8" },
-    method: "POST",
-    body: JSON.stringify({ user_email }),
-  }).then((res) => res.json());
+  axios
+    .post(`${BASE_URL}/bopool/auth/registration/mail`, { user_email })
+    .then((res) => res.data);
