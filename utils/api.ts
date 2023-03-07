b5c68fea54004c887794bd75cf4e43608a92070d
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-07 17:40:24 +0900

    user_id로 키 명칭 통일

--- utils/api.ts
+++ utils/api.ts
@@ -7,9 +7,9 @@ const BASE_URL = "http://3.39.118.55:12023";
 axios.defaults.headers.common["Content-Type"] =
   "application/json; charset=utf-8";
 
-export const getOtp = (user_email: string) =>
+export const getOtp = (user_id: string) =>
   axios
-    .post(`${BASE_URL}/bopool/auth/registration/mail`, { user_email })
+    .post(`${BASE_URL}/bopool/auth/registration/mail`, { user_id })
     .then(({ data }) => data);
 
 export const signIn = (signInForm: SignInForm) =>
