babb68b59d4eea36fc9691ba759c8ace6b846215
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-22 01:07:51 +0900

    테마 오류 수정

--- App.tsx
+++ App.tsx
@@ -1,21 +1,27 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { AppRegistry } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  Provider as PaperProvider,
+  useTheme,
+} from "react-native-paper";
 import { name as appName } from "./app.json";
 import * as SplashScreen from "expo-splash-screen";
 import * as Font from "expo-font";
 import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
 import { StatusBar } from "expo-status-bar";
 import Root from "./navigation/Root";
-import {
-  DarkTheme,
-  DefaultTheme,
-  NavigationContainer,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "styled-components/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useColorScheme } from "react-native";
-import { darkTheme, lightTheme } from "./styles/theme";
+import {
+  DarkTheme,
+  DefaultTheme,
+  styledDarkTheme,
+  styledLightTheme,
+} from "./styles/theme";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useSetRecoilState } from "recoil";
 import { loginAtom } from "./utils/recoil";
@@ -65,12 +71,12 @@ export default function App() {
   } else {
     return (
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <ThemeProvider theme={isDark ? styledDarkTheme : styledLightTheme}>
           <NavigationContainer
             onReady={onLayoutRootView}
             theme={isDark ? DarkTheme : DefaultTheme}
           >
-            <PaperProvider>
+            <PaperProvider theme={isDark ? MD3DarkTheme : MD3LightTheme}>
               <Root />
               <StatusBar style="auto" />
             </PaperProvider>
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,9 +1,35 @@
-export const lightTheme = {
+import { Theme } from "@react-navigation/native";
+
+export const styledLightTheme = {
   bgColor: "#fff",
   textColor: "#333",
 };
 
-export const darkTheme = {
+export const styledDarkTheme = {
   bgColor: "#111",
   textColor: "#fff",
+};
+
+export const DarkTheme: Theme = {
+  dark: true,
+  colors: {
+    primary: "rgb(10, 132, 255)",
+    background: "rgb(28, 27, 31)",
+    card: "rgb(18, 18, 18)",
+    text: "rgb(229, 229, 231)",
+    border: "rgb(39, 39, 41)",
+    notification: "rgb(255, 69, 58)",
+  },
+};
+
+export const DefaultTheme: Theme = {
+  dark: false,
+  colors: {
+    primary: "rgb(0, 122, 255)",
+    background: "rgb(255, 251, 254)",
+    card: "rgb(255, 255, 255)",
+    text: "rgb(28, 28, 30)",
+    border: "rgb(216, 216, 216)",
+    notification: "rgb(255, 59, 48)",
+  },
 };
