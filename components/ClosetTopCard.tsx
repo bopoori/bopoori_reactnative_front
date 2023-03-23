2ee110582487e6c05504e18b16b10bfb14ea3509
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-03-23 15:11:28 +0900

    관리 버튼 페이지 연결

--- components/ClosetTopCard.tsx
+++ components/ClosetTopCard.tsx
@@ -1,14 +1,32 @@
+import { MaterialBottomTabNavigationProp } from "@react-navigation/material-bottom-tabs";
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { useState } from "react";
 import { StyleSheet } from "react-native";
 import { Avatar, Card, Button } from "react-native-paper";
+import { RootParamList, TabsParamList } from "../navigation/Root";
 
 const TopCard = () => {
   const [show, setShow] = useState(true);
-  const closeCard = () => setShow(false);
+  const { navigate } =
+    useNavigation<
+      CompositeNavigationProp<
+        MaterialBottomTabNavigationProp<TabsParamList, "Closet">,
+        NativeStackNavigationProp<RootParamList>
+      >
+    >();
+  // const closeCard = () => setShow(false);
 
   const LeftContent = (props: { size: number }) => (
     <Avatar.Icon {...props} icon="tshirt-crew" />
   );
+
+  const onButtonPress = () => {
+    navigate("Stack", { screen: "ClosetSettings" });
+  };
 
   return show ? (
     <Card style={style.card}>
@@ -18,7 +36,7 @@ const TopCard = () => {
         left={LeftContent}
       />
       <Card.Actions>
-        <Button onPress={() => console.log("hello")} mode="contained">
+        <Button onPress={onButtonPress} mode="contained">
           관리
         </Button>
         {/* <Button onPress={closeCard} mode="outlined">
