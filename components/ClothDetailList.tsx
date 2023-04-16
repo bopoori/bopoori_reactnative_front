d775d256206b1f15d8ec1b02249ebe9ca0d053bd
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-16 13:45:59 +0900

    옷 정보 항목 컴포넌트 분리

---
+++ components/ClothDetailList.tsx
@@ -0,0 +1,148 @@
+import { Dimensions } from "react-native";
+import { List, useTheme } from "react-native-paper";
+import styled from "styled-components/native";
+import {
+  ClothAction,
+  ClothStateType,
+  DialogName,
+} from "../utils/clothReducers";
+import { clothCategories } from "../utils/clothCategories";
+import SelectDialog from "./SelectDialog";
+import InputDialog from "./InputDialog";
+const { width: WINDOW_WIDTH } = Dimensions.get("window");
+
+interface ClothDetailListProps {
+  uri: string;
+  state: ClothStateType;
+  dispatch: React.Dispatch<ClothAction>;
+}
+
+const ClothDetailList: React.FC<ClothDetailListProps> = ({
+  uri,
+  state,
+  dispatch,
+}) => {
+  const theme = useTheme();
+  const closeListDialog = () => dispatch({ type: "CLOSE_LIST_DIALOG" });
+  const closeInputDialog = () => dispatch({ type: "CLOSE_INPUT_DIALOG" });
+  const openListDialog = (dialogName: "category" | "color") => {
+    dispatch({
+      type: "OPEN_LIST_DIALOG",
+      payload: { lists: clothCategories.allTitles[dialogName], dialogName },
+    });
+  };
+  const openInputDialog = (
+    dialogName: "brand" | "buy_date" | "price" | "explain" | "name"
+  ) => {
+    dispatch({ type: "OPEN_INPUT_DIALOG", payload: { dialogName } });
+  };
+  const onPressListSave = (dialogName: DialogName, value: string) =>
+    dispatch({ type: "SAVE_LIST_INFO", payload: { dialogName, value } });
+  const onPressInputSave = (dialogName: DialogName, value: string) =>
+    dispatch({ type: "SAVE_INPUT_INFO", payload: { dialogName, value } });
+
+  return (
+    <>
+      <SelectDialog
+        initialValue={state.info[state.dialogName]}
+        title={clothCategories.allTypes[state.dialogName]}
+        dialogName={state.dialogName}
+        visible={state.listDialog.status}
+        lists={state.listDialog.lists}
+        onPressSave={onPressListSave}
+        closeDialog={closeListDialog}
+      />
+      <InputDialog
+        initialValue={state.info[state.dialogName]}
+        title={clothCategories.allTypes[state.dialogName]}
+        dialogName={state.dialogName}
+        visible={state.inputDialog.status}
+        onPressSave={onPressInputSave}
+        closeDialog={closeInputDialog}
+      />
+      <Container>
+        <ImageWrapper>
+          <PreviewImage source={{ uri }} resizeMode="contain" />
+        </ImageWrapper>
+        <ListSection
+          title="옷에 대한 필수 정보"
+          titleStyle={{ color: theme.colors.primary, marginLeft: 8 }}
+        >
+          <ListItem
+            descriptionStyle={descriptionStyle}
+            title="옷 이름"
+            onPress={() => openInputDialog("name")}
+            description={description(state.info.name)}
+          />
+          <ListItem
+            descriptionStyle={descriptionStyle}
+            title="카테고리"
+            onPress={() => openListDialog("category")}
+            description={description(state.info.category)}
+          />
+          <ListItem
+            descriptionStyle={descriptionStyle}
+            title="색상"
+            onPress={() => openListDialog("color")}
+            description={description(state.info.color)}
+          />
+          <ListItem
+            descriptionStyle={descriptionStyle}
+            title="브랜드"
+            onPress={() => openInputDialog("brand")}
+            description={description(state.info.brand)}
+          />
+        </ListSection>
+        <List.Accordion
+          title="더 자세한 내용 기록하기"
+          titleStyle={{ marginLeft: 8 }}
+        >
+          <ListItem
+            descriptionStyle={descriptionStyle}
+            title="구매일"
+            onPress={() => openInputDialog("buy_date")}
+            description={description(state.info.buy_date)}
+          />
+          <ListItem
+            descriptionStyle={descriptionStyle}
+            title="구매가격"
+            onPress={() => openInputDialog("price")}
+            description={description(state.info.price)}
+          />
+          <ListItem
+            descriptionStyle={descriptionStyle}
+            title="설명"
+            onPress={() => openInputDialog("explain")}
+            description={description(state.info.explain)}
+          />
+        </List.Accordion>
+      </Container>
+    </>
+  );
+};
+
+function description(text: string) {
+  return text === "" ? "없음" : text;
+}
+const Container = styled.ScrollView``;
+const ImageWrapper = styled.View`
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+const PreviewImage = styled.Image`
+  width: ${(WINDOW_WIDTH / 10) * 9}px;
+  height: ${(WINDOW_WIDTH / 10) * 9}px;
+`;
+const ListSection = styled(List.Section)`
+  padding-top: 0;
+`;
+const ListItem = styled(List.Item)`
+  padding-left: 8px;
+`;
+const descriptionStyle = { paddingTop: 8 };
+const EmptySpace = styled.View`
+  padding-bottom: 50px;
+`;
+
+export default ClothDetailList;
--- screens/stacks/AddNewCloth.tsx
+++ screens/stacks/AddNewCloth.tsx
@@ -4,26 +4,18 @@ import {
   ActivityIndicator,
   Appbar,
   Button,
-  List,
   useTheme,
 } from "react-native-paper";
 import styled from "styled-components/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Alert, Dimensions, Image, Platform } from "react-native";
+import { Alert, Platform } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { StackParamList } from "../../navigation/Root";
-import SelectDialog from "../../components/SelectDialog";
-import {
-  clothReducer,
-  CLOTH_STATE,
-  DialogName,
-} from "../../utils/clothReducers";
-import InputDialog from "../../components/InputDialog";
+import { clothReducer, CLOTH_STATE } from "../../utils/clothReducers";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { getCategoryLists, uploadCloth } from "../../utils/api";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { clothCategories } from "../../utils/clothCategories";
-const { width: WINDOW_WIDTH } = Dimensions.get("window");
+import ClothDetailList from "../../components/ClothDetailList";
 
 type Props = NativeStackScreenProps<StackParamList, "AddNewCloth">;
 
@@ -38,24 +30,6 @@ const AddNewCloth: React.FC<Props> = ({
   const [imageState, setImageState] =
     useState<ImagePicker.ImagePickerAsset>(image);
   const [state, dispatch] = useReducer(clothReducer, CLOTH_STATE);
-
-  const closeListDialog = () => dispatch({ type: "CLOSE_LIST_DIALOG" });
-  const closeInputDialog = () => dispatch({ type: "CLOSE_INPUT_DIALOG" });
-  const openListDialog = (dialogName: "category" | "color") => {
-    dispatch({
-      type: "OPEN_LIST_DIALOG",
-      payload: { lists: clothCategories.allTitles[dialogName], dialogName },
-    });
-  };
-  const openInputDialog = (
-    dialogName: "brand" | "buy_date" | "price" | "explain" | "name"
-  ) => {
-    dispatch({ type: "OPEN_INPUT_DIALOG", payload: { dialogName } });
-  };
-  const onPressListSave = (dialogName: DialogName, value: string) =>
-    dispatch({ type: "SAVE_LIST_INFO", payload: { dialogName, value } });
-  const onPressInputSave = (dialogName: DialogName, value: string) =>
-    dispatch({ type: "SAVE_INPUT_INFO", payload: { dialogName, value } });
 
   const { isLoading: categoryLoading, data: categoryData } = useQuery({
     queryKey: ["categories"],
@@ -82,10 +56,9 @@ const AddNewCloth: React.FC<Props> = ({
   };
 
   const postNewCloth = async () => {
-    console.log("post new cloth clicked!");
     const loginData = await AsyncStorage.getItem("loginData");
     const closet_number = await AsyncStorage.getItem("closetSequence");
-    const user_number = JSON.parse(loginData).user_uid;
+    const user_number = JSON.parse(loginData!).user_uid;
 
     if (user_number && closet_number) {
       const uploadClothForm = {
@@ -95,16 +68,15 @@ const AddNewCloth: React.FC<Props> = ({
         table_name: state.info.category.toLowerCase(),
       };
       const formData = createFormData(imageState!, uploadClothForm);
-      console.log("formData", formData);
-      const key = { formData, user_number };
+      const uploadArgs = { formData, user_number };
       try {
-        const res = await uploadAsync(key);
+        const res = await uploadAsync(uploadArgs);
         Alert.alert(JSON.stringify(res));
       } catch {
         console.error;
       }
     } else {
-      console.log("no!!!");
+      Alert.alert("고객 정보를 확인할 수 없습니다. 다시 로그인 해주세요.");
     }
   };
 
@@ -127,105 +99,16 @@ const AddNewCloth: React.FC<Props> = ({
   }
   return (
     <>
-      <SelectDialog
-        initialValue={state.info[state.dialogName]}
-        title={clothCategories.allTypes[state.dialogName]}
-        dialogName={state.dialogName}
-        visible={state.listDialog.status}
-        lists={state.listDialog.lists}
-        onPressSave={onPressListSave}
-        closeDialog={closeListDialog}
-      />
-      <InputDialog
-        initialValue={state.info[state.dialogName]}
-        title={clothCategories.allTypes[state.dialogName]}
-        dialogName={state.dialogName}
-        visible={state.inputDialog.status}
-        onPressSave={onPressInputSave}
-        closeDialog={closeInputDialog}
-      />
       <Appbar.Header style={{ elevation: 1 }}>
         <Appbar.BackAction onPress={goBack} />
         <Appbar.Content title="새로운 옷 추가" />
       </Appbar.Header>
-      <ScrollContainer>
-        <ImageBox>
-          <Image
-            source={{ uri: imageState.uri }}
-            style={{
-              width: (WINDOW_WIDTH / 10) * 9,
-              height: (WINDOW_WIDTH / 10) * 9,
-            }}
-            resizeMode="contain"
-          />
-        </ImageBox>
-        <ListSection
-          title="옷에 대한 정보"
-          titleStyle={{ color: theme.colors.primary, marginLeft: 8 }}
-        >
-          <ListItem
-            descriptionStyle={descriptionStyle}
-            title="옷 이름"
-            onPress={() => openInputDialog("name")}
-            description={state.info.name === "" ? "없음" : state.info.name}
-          />
-          <ListItem
-            descriptionStyle={descriptionStyle}
-            title="카테고리"
-            onPress={() => openListDialog("category")}
-            description={
-              state.info.category === "" ? "없음" : state.info.category
-            }
-          />
-          <ListItem
-            descriptionStyle={descriptionStyle}
-            title="색상"
-            onPress={() => openListDialog("color")}
-            description={state.info.color === "" ? "없음" : state.info.color}
-          />
-          <ListItem
-            descriptionStyle={descriptionStyle}
-            title="브랜드"
-            onPress={() => openInputDialog("brand")}
-            description={state.info.brand === "" ? "없음" : state.info.brand}
-          />
-        </ListSection>
-        <List.Accordion
-          title="더 자세한 내용 기록하기"
-          titleStyle={{ marginLeft: 8 }}
-        >
-          <ListItem
-            descriptionStyle={descriptionStyle}
-            title="구매일"
-            onPress={() => openInputDialog("buy_date")}
-            description={
-              state.info.buy_date === "" ? "없음" : state.info.buy_date
-            }
-          />
-          <ListItem
-            descriptionStyle={descriptionStyle}
-            title="구매가격"
-            onPress={() => openInputDialog("price")}
-            description={state.info.price === "" ? "없음" : state.info.price}
-          />
-          <ListItem
-            descriptionStyle={descriptionStyle}
-            title="설명"
-            onPress={() => openInputDialog("explain")}
-            description={
-              state.info.explain === "" ? "없음" : state.info.explain
-            }
-          />
-        </List.Accordion>
-        <Btns>
-          <Btn mode="outlined" onPress={openCamera}>
-            다시 찍기
-          </Btn>
-          <Btn mode="contained" onPress={postNewCloth} loading={uploadLoading}>
-            옷장에 추가
-          </Btn>
-        </Btns>
-      </ScrollContainer>
+      <ClothDetailList uri={imageState.uri} state={state} dispatch={dispatch} />
+      <Btns bg={theme.colors.primaryContainer}>
+        <Btn mode="contained" onPress={postNewCloth} loading={uploadLoading}>
+          옷장에 저장
+        </Btn>
+      </Btns>
     </>
   );
 };
@@ -235,29 +118,15 @@ const LoadingWrapper = styled.View`
   justify-content: center;
   align-items: center;
 `;
-const descriptionStyle = { paddingTop: 8 };
-const ScrollContainer = styled.ScrollView`
-  flex: 1;
-`;
-const ImageBox = styled.View`
-  padding: 20px;
-  justify-content: center;
-  align-items: center;
-`;
-const ListSection = styled(List.Section)`
-  padding-top: 0;
-`;
-const ListItem = styled(List.Item)`
-  padding-left: 8px;
-`;
-const Btns = styled.View`
+const Btns = styled.View<{ bg: string }>`
+  background-color: ${({ bg }) => bg};
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
   padding-top: 20px;
   padding-bottom: 50px;
 `;
 const Btn = styled(Button)`
-  margin: 0 24px;
+  padding: 0 24px;
 `;
 
 export default AddNewCloth;
