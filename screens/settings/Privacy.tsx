9048c3ce3f51142157071590770499ba90711c5c
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-04-25 22:45:44 +0900

    개인정보처리방침 노션 웹뷰 추가

--- screens/settings/Privacy.tsx
+++ screens/settings/Privacy.tsx
@@ -1,7 +1,11 @@
 import { useNavigation } from "@react-navigation/native";
-import { Appbar } from "react-native-paper";
+import { useState } from "react";
+import { ActivityIndicator, Appbar } from "react-native-paper";
+import WebView from "react-native-webview";
 
 const Privacy = () => {
+  const [loading, setLoading] = useState(true);
+  const endLoading = () => setLoading(false);
   const { goBack } = useNavigation();
   return (
     <>
@@ -9,6 +13,14 @@ const Privacy = () => {
         <Appbar.BackAction onPress={goBack} />
         <Appbar.Content title="개인정보처리방침" />
       </Appbar.Header>
+      {loading ? <ActivityIndicator animating /> : null}
+      <WebView
+        onLoadEnd={endLoading}
+        scalesPageToFit
+        source={{
+          uri: "https://yxxnhex.notion.site/e486a7e58e994e80844fa776af21c11e",
+        }}
+      />
     </>
   );
 };
