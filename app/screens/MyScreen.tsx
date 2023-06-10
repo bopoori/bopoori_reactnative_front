ce7198b085f3b473bea32d4ab5a0514c7660888f
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-10 15:24:26 +0900

    로그아웃 함수 분리

--- app/screens/MyScreen.tsx
+++ app/screens/MyScreen.tsx
@@ -3,18 +3,25 @@ import { Button, ListItem, Screen } from "app/components"
 import { useStores } from "app/models"
 import { spacing } from "app/theme"
 import { TextStyle, View, ViewStyle } from "react-native"
+import { BopoolTabScreenProps } from "app/navigators"
 
-export const MyScreen = () => {
+interface MyScreenProps extends BopoolTabScreenProps<"My"> {}
+
+export const MyScreen: React.FC<MyScreenProps> = () => {
   const {
     authenticationStore: { logout },
   } = useStores()
+
+  const onLogoutPress = () => {
+    logout()
+  }
   return (
     <Screen preset="scroll" safeAreaEdges={["top", "bottom"]} contentContainerStyle={$container}>
       {["알람", "내 정보", "옷장 설정", "개인정보처리방침", "이용약관"].map((i) => (
         <ListItem key={i} text={i} textStyle={$listItem} />
       ))}
       <View style={$buttonContainer}>
-        <Button style={$button} tx="common.logOut" onPress={logout} />
+        <Button style={$button} tx="common.logOut" onPress={onLogoutPress} />
       </View>
     </Screen>
   )
