590d37d1d73d358df377da8926bf2aba352e811e
정형진 <88423849+KeMezz@users.noreply.github.com>
2023-06-10 15:23:16 +0900

    userStore 청소, 회원가입 버튼 역할 추가

--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -1,11 +1,12 @@
 import { observer } from "mobx-react-lite"
-import React, { FC } from "react"
+import React, { useEffect } from "react"
 import { Platform, Image, ImageStyle, TextStyle, View, ViewStyle } from "react-native"
 import { Button, Text } from "../components"
 import { colors, spacing } from "../theme"
 import { useSafeAreaInsetsStyle } from "../utils/useSafeAreaInsetsStyle"
 import { AppStackScreenProps } from "app/navigators"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { useStores } from "app/models"
 
 const isIOS = Platform.OS === "ios"
 const welcomeLogo = require("../../assets/images/logo.png")
@@ -16,12 +17,24 @@ const KakaoLoginLeftAccessory = () => (
   <MaterialCommunityIcons size={22} name="chat" color="#100F01" style={$kakaoLeftAccesory} />
 )
 
-export const WelcomeScreen: FC<WelcomeScreenProps> = observer(function WelcomeScreen({
+export const WelcomeScreen: React.FC<WelcomeScreenProps> = observer(function WelcomeScreen({
   navigation,
 }) {
   const $bottomContainerInsets = useSafeAreaInsetsStyle(["bottom"])
+  const {
+    userStore: { cleanUp },
+  } = useStores()
+
+  useEffect(() => {
+    cleanUp()
+  }, [])
+
   function goNext() {
     navigation.navigate("Login")
+  }
+
+  function goSignUp() {
+    navigation.navigate("BodyInfoScreen")
   }
 
   return (
@@ -38,14 +51,14 @@ export const WelcomeScreen: FC<WelcomeScreenProps> = observer(function WelcomeSc
       </View>
 
       <View style={[$bottomContainer, $bottomContainerInsets]}>
-        <Button testID="login-button" tx="welcomeScreen.signUp" />
+        <Button onPress={goSignUp} tx="welcomeScreen.signUp" />
         <Button
           tx="welcomeScreen.kakaoLogin"
           preset="kakao"
           LeftAccessory={KakaoLoginLeftAccessory}
         />
         <Button
-          testID="next-screen-button"
+          testID="login-button"
           preset="reversed"
           tx="welcomeScreen.emailLogin"
           onPress={goNext}
